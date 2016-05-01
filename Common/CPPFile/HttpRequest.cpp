6fc693009a7e9e2ef044afab68baad172ee44ee5
dyno <oldman.mtb@gmail.com>
2016-04-30 23:09:44 -0700

    add curl, get exchange rate by http

---
+++ Common/CPPFile/HttpRequest.cpp
@@ -0,0 +1,9 @@
+//
+//  HttpRequest.cpp
+//  USNIT
+//
+//  Created by dyno on 4/24/16.
+//  Copyright © 2016 dyno. All rights reserved.
+//
+
+#include "HttpRequest.hpp"
---
+++ Common/CPPFile/HttpRequest.hpp
@@ -0,0 +1,14 @@
+//
+//  HttpRequest.hpp
+//  USNIT
+//
+//  Created by dyno on 4/24/16.
+//  Copyright © 2016 dyno. All rights reserved.
+//
+
+#ifndef HttpRequest_hpp
+#define HttpRequest_hpp
+
+#include <stdio.h>
+
+#endif /* HttpRequest_hpp */
